0a8535eca777c679e09372baa5cdbcd691305423
Luiz Eduardo <dudu.christo@gmail.com>
2018-02-20 14:24:54 +0100

    CJS-08 - Replaces QUnit for Mocha and do refactoring

--- currency.js
+++ currency.js
@@ -1,91 +1,85 @@
-var CurrencyInfo = (function() {
-    var currencies =
-        {
-            de_de: {
-                country: 'Deutschland',
-                symbol: '€',
-                decimalSeparator: '.'
-            },
+  const currencies = {
+    de_de: {
+        country: 'Deutschland',
+        symbol: '€',
+        decimalSeparator: '.'
+    },
 
-            de_ch: {
-                country: 'Schweiz',
-                symbol: 'Fr',
-                decimalSeparator: ','
-            },
+    de_ch: {
+        country: 'Schweiz',
+        symbol: 'Fr',
+        decimalSeparator: ','
+    },
 
-            en_au: {
-                country: 'Australia / New Zealand',
-                symbol: 'AU$',
-                decimalSeparator: '.'
-            },
+    en_au: {
+        country: 'Australia / New Zealand',
+        symbol: 'AU$',
+        decimalSeparator: '.'
+    },
 
-            en_ca: {
-                country: 'Canada',
-                symbol: 'CA$',
-                decimalSeparator: '.'
-            },
+    en_ca: {
+        country: 'Canada',
+        symbol: 'CA$',
+        decimalSeparator: '.'
+    },
 
-            en_uk:  {
-                country: 'United Kingdom',
-                symbol: '£',
-                decimalSeparator: '.'
-            },
+    en_uk:  {
+        country: 'United Kingdom',
+        symbol: '£',
+        decimalSeparator: '.'
+    },
 
-            en_us : {
-                country: 'USA',
-                symbol: '$',
-                decimalSeparator: '.'
-            },
+    en_us : {
+        country: 'USA',
+        symbol: '$',
+        decimalSeparator: '.'
+    },
 
-            es_es: {
-                country: 'España',
-                symbol: '€',
-                decimalSeparator: '.'
-            },
+    es_es: {
+        country: 'España',
+        symbol: '€',
+        decimalSeparator: '.'
+    },
 
-            it_it: {
-                country: 'Italia',
-                symbol: '€',
-                decimalSeparator: '.'
-            },
+    it_it: {
+        country: 'Italia',
+        symbol: '€',
+        decimalSeparator: '.'
+    },
 
-            ja: {
-                country: 'Japan',
-                symbol: '¥',
-                decimalSeparator: '.'
-            },
+    ja: {
+        country: 'Japan',
+        symbol: '¥',
+        decimalSeparator: '.'
+    },
 
-            fr_fr: {
-                country: 'France',
-                symbol: '€',
-                decimalSeparator: '.'
-            }
-    };
-
-    var defaultCurrency = currencies.de_de;
-
-    var getCurrencyPattern = function(location) {
-
-        if (!isLocationValid(location)) return null;
-        
-        //no location found: return the default
-        if (typeof currencies[location] === 'undefined')
-            return defaultCurrency;
- 
-        return currencies[location];
-    };
-
-    function isLocationValid(location) {
-        
-        if ( location == null ||
-            location.length > 5 ||
-            location.length === 0)
-             return false;
-
-         return true;
+    fr_fr: {
+        country: 'France',
+        symbol: '€',
+        decimalSeparator: '.'
     }
+};
 
-    return {
-        getCurrencyPattern: getCurrencyPattern
-    };
-})();
+const defaultCurrency = currencies.de_de;
+
+const getCurrencyPattern = (location) => {
+    if (!isLocationValid(location)) return null;
+
+    //no location found: return the default
+    if (typeof currencies[location] === 'undefined')
+        return defaultCurrency;
+
+    return currencies[location];
+};
+
+const isLocationValid = (location) => {
+    if (location == null ||
+        location.length > 5 ||
+        location.length === 0)
+         return false;
+     return true;
+};
+
+module.exports = {
+    getCurrencyPattern: getCurrencyPattern
+};
---
+++ tests/currency.spec.js
@@ -0,0 +1,111 @@
+const { assert } = require('chai');
+const CurrencyInfo = require('../currency');
+
+describe("CurrencyInfo",() => {
+   describe("getCurrencyPattern()", () => {
+       it("should returns an object with correct values for valid location", () => {
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('de_de'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('de_ch'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('en_au'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('en_ca'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('en_uk'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('en_us'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('es_es'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('it_it'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('ja'), 'object');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('fr_fr'), 'object');
+       });
+
+       it('should return null for invalid location',() => {
+           assert.typeOf(CurrencyInfo.getCurrencyPattern(''), 'null');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern('test_invalid'), 'null');
+           assert.typeOf(CurrencyInfo.getCurrencyPattern(null), 'null');
+           assert.equal(CurrencyInfo.getCurrencyPattern('invalid'), null);
+       });
+
+       it('should return default location for non implemented location',() => {
+           let ci = CurrencyInfo.getCurrencyPattern('eu_pt');
+           assert.typeOf(ci, 'object');
+           assert.property(ci, 'country');
+           assert.equal(ci.country, 'Deutschland')
+           assert.property(ci, 'symbol');
+           assert.equal(ci.symbol, '€')
+           assert.property(ci, 'decimalSeparator');
+           assert.equal(ci.decimalSeparator, '.')
+       });
+
+       it("should return correct values for location Germany (de_de)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('de_de');
+           assert.isNotNull(ci);
+           assert.equal(ci.country,"Deutschland");
+           assert.equal(ci.symbol,"€" );
+           assert.equal(ci.decimalSeparator,".");
+       });
+
+
+       it("should return correct values for location Switzerland (de_ch) ", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('de_ch');
+           assert.isNotNull(ci);
+           assert.equal(ci.country,"Schweiz");
+           assert.equal(ci.symbol,"Fr" );
+           assert.equal(ci.decimalSeparator,"," );
+       });
+
+       it("should return correct values for location Australia / New Zealand (en_au) ", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('en_au');
+           assert.isNotNull(ci);
+           assert.equal(ci.country,"Australia / New Zealand");
+           assert.equal(ci.symbol,"AU$" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location Canada (en_ca)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('en_ca');
+           assert.equal(ci.country,"Canada");
+           assert.equal(ci.symbol,"CA$" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location United Kingdom (en_uk)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('en_uk');
+           assert.equal(ci.country,"United Kingdom");
+           assert.equal(ci.symbol,"£" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location USA (en_us)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('en_us');
+           assert.equal(ci.country,"USA");
+           assert.equal(ci.symbol,"$" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location Italy (it_it)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('it_it');
+           assert.equal(ci.country,"Italia");
+           assert.equal(ci.symbol,"€" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location Spain (es_es)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('es_es');
+           assert.equal(ci.country,"España");
+           assert.equal(ci.symbol,"€" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+
+       it("should return correct values for location Japan (ja) ", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('ja');
+           assert.equal(ci.country,"Japan");
+           assert.equal(ci.symbol,"¥" );
+           assert.equal(ci.decimalSeparator,".");
+       });
+
+       it("should return correct values for location France (fr_fr)", () => {
+           let ci = CurrencyInfo.getCurrencyPattern('fr_fr');
+           assert.equal(ci.country,"France");
+           assert.equal(ci.symbol,"€" );
+           assert.equal(ci.decimalSeparator,"." );
+       });
+   });
+});
